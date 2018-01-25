f46e71c43be3837aacd9fd716f41f3272f7507a1
Gyles Batara <gyles.batara@live.com>
2018-01-24 16:43:42 -0800

    .

---
+++ InterviewFinder/Models/Contact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewFinder.Models
+{
+    [Table("Contacts")]
+    public class Contact
+    {
+        public Contact(string contactEmail, string contactEmailHash, string contactTwitter, string contactPhone, string contactFullName, 
+                       string contactAgeRange, string contactGender, string contactLocation, string contactTitle, string contactOrganization, 
+                      string contactLinkedIn, string contactFacebook, string contactBio, string contactAvatar, string contactWebsite, int contactId)
+        {
+            contactId = id;
+            contactEmail = Email;
+            contactEmailHash = EmailHash;
+            contactTwitter = Twitter;
+            contactPhone = PhoneNumber;
+            contactFullName = Name;
+            contactAgeRange = AgeRange;
+            contactGender = Gender;
+            contactLocation = Location;
+            contactTitle = JobTitle;
+            contactOrganization = Organization;
+            contactLinkedIn = LinkedIn;
+            contactFacebook = Facebook;
+
+        }
+
+        public Contact()
+        {
+            
+        }
+
+        [Key]
+        public int id { get; set; }
+		public string Email { get; set; }
+		public string EmailHash { get; set; }
+        public string Twitter { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Name { get; set; }
+		public string AgeRange { get; set; }
+		public string Gender { get; set; }
+		public string Location { get; set; }
+		public string JobTitle { get; set; }
+		public string Organization { get; set; }
+		public string LinkedIn { get; set; }
+		public string Facebook { get; set; }
+    }
+    //public static List<Contact> GetContacts() 
+    //{
+    //    //var client = new RestClient("")
+    //}
+}
---
+++ InterviewFinder/Models/EnvironmentVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace InterviewFinder.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "gBp93uWSwT5c204DeVWNXnAkmP9Yy1H9";
+    }
+}
